f94040343b28ed1974d02176db5c823f0fe162a9
Geetla Dheeraj Reddy <dheerajreddygeetla@gmail.com>
2026-08-18 21:04:35 +0530

    Time: 127 ms (5.11%), Space: 345.7 MB (5.18%) - LeetHub

--- 0108-convert-sorted-array-to-binary-search-tree/0108-convert-sorted-array-to-binary-search-tree.cpp
+++ 0108-convert-sorted-array-to-binary-search-tree/0108-convert-sorted-array-to-binary-search-tree.cpp
@@ -16,11 +16,13 @@ public:
     }
     TreeNode* convert(vector<int>nums,int left,int right){
         if(left>right) return nullptr;
+
         int mid=left+(right-left)/2;
         TreeNode* node=new TreeNode(nums[mid]);
+
         node->left=convert(nums,left,mid-1);
         node->right=convert(nums,mid+1,right);
-
+        
         return node;
     }
 };
